3cb6188d91351507d93e26c21fff3a4b4b770624
xing393939 <xing393939@users.noreply.github.com>
2021-07-27 10:31:43 +0800

    1

--- cpp/Strategy/main.cpp
+++ cpp/Strategy/main.cpp
@@ -8,8 +8,9 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+    Context * cxt = new Context();
+
 	Strategy * s1 = new ConcreteStrategyA();
-	Context * cxt = new Context();
 	cxt->setStrategy(s1);
 	cxt->algorithm();
 
@@ -19,21 +20,7 @@ int main(int argc, char *argv[])
 	
 	delete s1;
 	delete s2;
-	
-	int rac1 = 0x1;
-	int rac2 = 0x2;
-	int rac3 = 0x4;
-	int rac4 = 0x8;
-	
-	int i = 0xe;
-	int j = 0x5;
+	delete cxt;
 
-	int r1 = i & rac1;
-	int r2 = i & rac2;
-	int r3 = i & rac3;
-	int r4 = i & rac4;
-	
-	cout <<"res:" << r1 << "/" << r2 << "/" << r3 << "/" << r4 << endl;
-	
 	return 0;
 }
