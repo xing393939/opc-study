dd87d07c60f3a58b63f3b2d17da55299f7af7ffc
chenxing <chenxing@bookan.com.cn>
2021-08-16 20:17:52 +0800

    1

---
+++ algorithm/src/RadixSort.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+
+// 基数排序
+public class RadixSort {
+    public static void radixSort(int[] arr) {
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        // 从个位开始，对数组arr按"指数"进行排序
+        for (int exp = 1; max / exp > 0; exp *= 10) {
+            countingSort(arr, exp);
+        }
+    }
+
+    public static void countingSort(int[] arr, int exp) {
+        if (arr.length <= 1) {
+            return;
+        }
+
+        // 计算每个元素的个数
+        int[] c = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            c[(arr[i] / exp) % 10]++;
+        }
+
+        // 计算排序后的位置
+        for (int i = 1; i < c.length; i++) {
+            c[i] += c[i - 1];
+        }
+
+        // 临时数组r，存储排序之后的结果
+        int[] r = new int[arr.length];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            r[c[(arr[i] / exp) % 10] - 1] = arr[i];
+            c[(arr[i] / exp) % 10]--;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = r[i];
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = new int[]{5, 2, 7, 4, 1, 6, 3, 8};
+        RadixSort.radixSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
