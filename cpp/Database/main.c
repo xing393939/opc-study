ded5a8c6c3b1afd5d1180c1aa5b1301daed1aaa2
chenxing <chenxing@bookan.com.cn>
2021-10-19 14:51:34 +0800

    Database/main.c

---
+++ cpp/Database/main.c
@@ -0,0 +1,62 @@
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define PROFILE_FUNC(func) \
+{ long start = clock(); func(); printf("---%.3f---\n", (double) (clock() - start) / 1000); }
+#define PROFILE_FUNC_ARG1(func, arg1) \
+{ long start = clock(); func(arg1); printf("---%.3f---\n", (double) (clock() - start) / 1000); }
+
+typedef struct Stu {
+    char name[50];
+    int id;
+} Stu;
+
+void my_fwrite(char *path) {
+    FILE *fp = NULL;
+
+    //"w+"，读写方式打开，如果文件不存在，则创建; 如果文件存在，清空内容，再写
+    fp = fopen(path, "w+");
+    if (fp == NULL) {
+        perror("my_fwrite fopen");
+        return;
+    }
+
+    Stu s[3000];
+    char buf[50];
+    for (int i = 0; i < 3000; i++) {
+        sprintf(buf, "stu%d%d%d", i, i, i);
+        strcpy(s[i].name, buf);
+        s[i].id = i + 1;
+    }
+
+    unsigned long long ret = fwrite(s, sizeof(Stu), 3000, fp);
+    printf("ret_write = %llu\n", ret);
+
+    fclose(fp);
+}
+
+void my_fread(char *path) {
+    FILE *fp = NULL;
+    fp = fopen(path, "r+");
+    if (fp == NULL) {
+        perror("my_fread fopen");
+        return;
+    }
+
+    Stu s[3000];
+    unsigned long long ret = fread(s, sizeof(Stu), 3000, fp);
+    printf("ret_read = %llu\n", ret);
+
+    for (int i = 0; i < 3000; i++) {
+        printf("s[%d] = %s, %d\n", i, s[i].name, s[i].id);
+    }
+
+    fclose(fp);
+}
+
+int main() {
+    char *f = "./004.txt";
+    PROFILE_FUNC_ARG1(my_fwrite, f);
+    PROFILE_FUNC_ARG1(my_fread, f);
+}
