19fa2144f86cf8bc06fc3d2273f83dfd8269d855
xing393939 <xing393939@users.noreply.github.com>
2021-10-19 16:49:44 +0800

    1

--- cpp/Database/main.c
+++ cpp/Database/main.c
@@ -4,10 +4,8 @@
 #include <sys/mman.h>
 #include <fcntl.h>
 
-#define PROFILE_FUNC(func) \
-{ long start = clock(); func(); printf("---%.3f---\n", (double) (clock() - start) / 1000); }
-#define PROFILE_FUNC_ARG1(func, arg1) \
-{ long start = clock(); func(arg1); printf("---%.3f---\n", (double) (clock() - start) / 1000); }
+#define PROFILE_FUNC(func, ...) \
+{ long start = clock(); func(__VA_ARGS__); printf("=== %.3fs for " #func " ===\n", (double) (clock() - start) / 1000); }
 
 typedef struct Stu {
     char name[50];
@@ -25,7 +23,7 @@ void my_fwrite(char *path) {
     }
 
     Stu s[3000];
-    char buf[50];
+    char buf[50] = {0};
     for (int i = 0; i < 3000; i++) {
         sprintf(buf, "stu%d%d%d", i, i, i);
         strcpy(s[i].name, buf);
@@ -63,7 +61,7 @@ void mmap_read(char *path) {
     int fd;
     Stu *s;
     fd = open(path, O_RDWR);
-    s = mmap(NULL, ln, PROT_READ, MAP_PRIVATE, fd, 0);
+    s = mmap(NULL, ln, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     if (s == MAP_FAILED)
         return;
     for (int i = 0; i < 3000; i++) {
@@ -71,13 +69,27 @@ void mmap_read(char *path) {
     }
     printf("\n");
 
+    // 内存修改第0条记录
+    strcpy(s[0].name, "aaa000");
+
     munmap(s, ln);
     close(fd);
 }
 
+void func1() {
+    sleep(1);
+}
+
+void func2(int a, int b) {
+    sleep(b);
+}
+
 int main() {
     char *f = "./004.txt";
-    PROFILE_FUNC_ARG1(my_fwrite, f);
-    PROFILE_FUNC_ARG1(mmap_read, f);
-    PROFILE_FUNC_ARG1(my_fread, f);
+    PROFILE_FUNC(my_fwrite, f);
+    PROFILE_FUNC(mmap_read, f);
+    PROFILE_FUNC(my_fread, f);
+
+    PROFILE_FUNC(func1);
+    PROFILE_FUNC(func2, 1, 3);
 }
