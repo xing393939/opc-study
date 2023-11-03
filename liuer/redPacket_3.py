8dd3f12c93d1698a9a3dcd3c9273454aacf958ad
chenxing <chenxing@bookan.com.cn>
2023-11-03 16:50:06 +0800

    1

--- liuer/redPacket_3.py
+++ liuer/redPacket_3.py
@@ -3,12 +3,42 @@ import torch
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from timeit import default_timer as timer
+from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import os
 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
 start = timer()
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu" if torch.cuda.is_available() else "cpu")
+
+scaler = StandardScaler(with_mean=True, with_std=True)
+scaler.mean_ = [
+    1.08554986e02,
+    1.29016179e02,
+    1.20727032e02,
+    1.13427473e02,
+    1.66467707e04,
+    1.54938078e03,
+    7.50659189e00,
+]
+scaler.var_ = [
+    2.00737537e03,
+    5.77710843e03,
+    6.25343194e03,
+    5.21506444e03,
+    1.29878058e06,
+    2.81938577e08,
+    1.50334197e02,
+]
+scaler.scale_ = [
+    4.48037428e01,
+    7.60072919e01,
+    7.90786440e01,
+    7.22154031e01,
+    1.13964055e03,
+    1.67910267e04,
+    1.22610846e01,
+]
 
 
 # 1.prepare dataset
@@ -16,7 +46,8 @@ class DiabetesDataset(Dataset):
     def __init__(self, filepath):
         xy = np.loadtxt(filepath, skiprows=1, delimiter=",", dtype=np.float32)
         self.len = xy.shape[0]
-        self.x_data = torch.from_numpy(xy[:, :-1]).to(device)
+        x_data = scaler.transform(xy[:, :-1])
+        self.x_data = torch.from_numpy(x_data).to(device)
         self.y_data = torch.from_numpy(xy[:, [-1]]).to(device)
 
     def __getitem__(self, index):
@@ -26,7 +57,7 @@ class DiabetesDataset(Dataset):
         return self.len
 
 
-dataset = DiabetesDataset("redPacket_2.csv")
+dataset = DiabetesDataset("redPacket_train.csv")
 train_loader = DataLoader(dataset=dataset, batch_size=64, shuffle=True)
 
 
@@ -55,7 +86,7 @@ optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
 epoch_list = []
 loss_list = []
 # 4.training cycle forward, backward, update
-for epoch in range(10000):
+for epoch in range(50):
     running_loss = 0.0
     running_i = 0
     for i, data in enumerate(train_loader, 0):
@@ -67,23 +98,26 @@ for epoch in range(10000):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        # print(inputs)
-    if epoch % 100 == 0:
-        print(
-            "%d %d loss: %.3f" % (epoch, running_i, running_loss / running_i),
-            model.linear2.weight.data,
-            model.linear2.bias.data,
-        )
-        epoch_list.append(epoch)
-        loss_list.append(loss.item())
+    print(
+        "%d %d loss: %.3f" % (epoch, running_i, running_loss / running_i),
+        model.linear2.weight.data,
+        model.linear2.bias.data,
+    )
+    epoch_list.append(epoch)
+    loss_list.append(running_loss / running_i)
 
 # 5.test
-print("w = ", model.linear2.weight.data)
-print("b = ", model.linear2.bias.data)
-x_test = torch.Tensor([113, 118, 202, 197, 16750, 40, 1.9]).to(device)
-y_test = model(x_test)
-print("y_pred = ", y_test.data)
 print("cost = ", timer() - start)
+dataset = DiabetesDataset("redPacket_test.csv")
+train_loader = DataLoader(dataset=dataset, batch_size=99999)
+with torch.no_grad():
+    for i, data in enumerate(train_loader, 0):
+        inputs, labels = data
+        y_pred = model(inputs)
+        y_pred = y_pred.cpu().numpy()
+        y_pred = (y_pred > 0.5).astype(np.float32)
+        num_right = np.sum(y_pred == labels.cpu().numpy())
+        print(num_right / len(labels))
 
 plt.plot(epoch_list, loss_list)
 plt.ylabel("loss")
