75c9c83b707b8b47b1d2a0a3305ed0aa321337d8
xing393939 <xing393939@users.noreply.github.com>
2023-11-03 17:07:34 +0800

    1

---
+++ liuer/normalize.py
@@ -0,0 +1,15 @@
+from sklearn.preprocessing import StandardScaler
+import torchvision.transforms as transforms
+import numpy as np
+import torch
+
+x_np = np.array([[1.5, -1., 2.],
+                [2., 0., 0.]])
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_np)
+print(x_train, scaler.mean_, scaler.var_)
+
+trans_func = transforms.Normalize((0.1307,), (0.3081,))
+x_train = trans_func(torch.from_numpy(x_np))
+print(x_train)
+
