3e9cbea7cbbc5ce650544a2f030c8f31c6f5a55e
xing393939 <xing393939@users.noreply.github.com>
2023-11-03 11:36:10 +0800

    1

--- liuer/test.php
+++ liuer/test.php
@@ -1,12 +1,18 @@
 <?php
 
+$a = file_get_contents("000.csv");
+$arr = explode("\n", $a);
+
 $data = "";
-foreach (range(10000000, 10005000) as $i => $str) {
-    $survivalTime = rand(1, 50);
-    $averageAnswerTime = rand(1, 50);
-    $phone = rand(13701111330, 18991111330);
-    $outcome = rand(0, 1);
-    $data .= $str . ",$phone,$survivalTime,$averageAnswerTime,$outcome\n";
+foreach ($arr as $i => $str) {
+    $row = explode(",", $str);
+    if (count($row) != 8 || $i == 0) {
+        continue;
+    }
+    $survivalTime = strtotime($row[5]) - strtotime($row[2]);
+    $averageAnswerTime = $row[6] / $row[7] / 1000;
+    $ip = substr($row[1], 0, 5);
+    $data .= str_replace('.', ',', $row[0]) . ",$ip,$survivalTime,$averageAnswerTime,0\n";
 }
 
 file_put_contents("0.csv", $data);
