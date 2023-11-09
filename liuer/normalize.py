336b9fdeffcc107e0fedbde73e556dca9ce9411d
chenxing <chenxing@bookan.com.cn>
2023-11-09 16:05:13 +0800

    1

--- liuer/normalize.py
+++ liuer/normalize.py
@@ -3,8 +3,7 @@ import torchvision.transforms as transforms
 import numpy as np
 import torch
 
-x_np = np.array([[1.5, -1., 2.],
-                [2., 0., 0.]])
+x_np = np.array([[1.5, -1.0, 2.0], [2.0, 0.0, 0.0]])
 scaler = StandardScaler()
 x_train = scaler.fit_transform(x_np)
 print(x_train, scaler.mean_, scaler.var_)
@@ -12,4 +11,3 @@ print(x_train, scaler.mean_, scaler.var_)
 trans_func = transforms.Normalize((0.1307,), (0.3081,))
 x_train = trans_func(torch.from_numpy(x_np))
 print(x_train)
-
