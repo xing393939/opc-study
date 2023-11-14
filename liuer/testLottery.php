245e4285e01f07a12ef7302aaf9fdbe4cfae0040
chenxing <chenxing@bookan.com.cn>
2023-11-14 18:10:20 +0800

    1

---
+++ liuer/testLottery.php
@@ -0,0 +1,74 @@
+<?php
+
+$config = [
+    ['sentNum' => 0, 'prob' => 10],
+    ['sentNum' => 0, 'prob' => 20],
+    ['sentNum' => 0, 'prob' => 30],
+    ['sentNum' => 0, 'prob' => 40],
+];
+
+$probArr = [];
+foreach ($config as $i => $item) {
+    $probArr[$i] = round($item["prob"] * 100);
+}
+
+testCase(100, 'getRand');
+echo "\n";
+testCase(100, 'getFairRand');
+
+function testCase($count, $func)
+{
+    global $probArr;
+    $winArr = [];
+    for ($i = 0; $i < $count; $i++) {
+        $winIndex = $func($probArr);
+        if (isset($winArr[$winIndex])) {
+            $winArr[$winIndex]++;
+        } else {
+            $winArr[$winIndex] = 1;
+        }
+    }
+
+    for ($i = 0; $i < 4; $i++) {
+        echo "item$i sentNum=", $winArr[$i], " prob=", $winArr[$i] / $count, "\n";
+    }
+}
+
+function getFairRand($probArr)
+{
+    global $config;
+    $sentNum = array_sum(array_column($config, 'sentNum'));
+    $waitedArr = [];
+    foreach ($config as $i => $item) {
+        // 当前是否满足了中奖概率
+        if ($sentNum > 0 && ($item['sentNum'] / $sentNum) * 100 < $item['prob']) {
+            $waitedArr[$i] = $item['prob'];
+        }
+    }
+    if (empty($waitedArr)) {
+        $winIndex = getRand($probArr);
+    } else {
+        $winIndex = getRand($waitedArr);
+    }
+    $config[$winIndex]['sentNum']++;
+    return $winIndex;
+}
+
+function getRand($proArr)
+{
+    $result = -1;
+    $proSum = array_sum($proArr);
+    if ($proSum < 1) {
+        return $result;
+    }
+    foreach ($proArr as $key => $proCur) {
+        $randNum = mt_rand(1, $proSum);
+        if ($randNum <= $proCur) {
+            $result = $key;
+            break;
+        } else {
+            $proSum -= $proCur;
+        }
+    }
+    return $result;
+}
