c8aa4425e4a07cd307779affe3efc52d260e25ac
xing393939 <xing393939@users.noreply.github.com>
2023-12-13 17:49:06 +0800

    1

---
+++ ai/chapter05_2.py
@@ -0,0 +1,21 @@
+import pandas as pd
+datafile_path = "data/fine_food_reviews_with_embeddings_1k.parquet"
+df = pd.read_parquet(datafile_path)
+df["category"] = df.Score.replace(
+    {1: "one", 2: "two", 3: "three", 4: "four", 5: "five"}
+)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuracy_score
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    list(df.embedding.values), df.category, test_size=0.2, random_state=42
+)
+clf = LogisticRegression()
+clf.fit(X_train, y_train)
+preds = clf.predict(X_test)
+probas = clf.predict_proba(X_test)
+report = classification_report(y_test, preds, zero_division=0)
+print(report)
---
+++ ai/chapter06.py
@@ -0,0 +1,3 @@
+import pandas as pd
+datafile_path = "data/20_newsgroup_with_embedding.parquet"
+df = pd.read_parquet(datafile_path)
